78b5292eb5a3d647df7f5d8fe5c48e74b1224664
Noah Manaigre <noahmanaigre@hotmail.com>
2025-02-07 01:29:17 -0600

    Completed more of the file structure and implemented code inside of the routes directory

---
+++ src/api/v1/services/controllers/routes/employeeRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from 'express';
+import { createEmployee, getAllEmployees, getEmployeeById, updateEmployee, deleteEmployee } from '../controllers/employeeController';
+
+const router: Router = express.Router();
+
+router.post('/', createEmployee);
+router.get('/', getAllEmployees);
+router.get('/:id', getEmployeeById);
+router.put('/:id', updateEmployee);
+router.delete('/:id', deleteEmployee);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 // Importing morgan
+import express from 'express';
 import morgan from "morgan";
+import employeeRoutes from './api/v1/routes/employeeRoutes';
+import branchRoutes from './api/v1/routes/branchRoutes';
+
+const app = express();
 
 // Use morgan for HTTP request logging
 app.use(morgan("combined"));
+
+app.use(express.json());
+app.use('/api/v1/employees', employeeRoutes);
+app.use('/api/v1/branches', branchRoutes);
 
 export default app;
